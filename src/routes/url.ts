f9ea754847bd0e6d8a4c6174c4247edd50f626e1
Naveed <46922100+nedanwr@users.noreply.github.com>
2021-07-20 15:44:11 +0500

    feat: Generate urlCode

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -2,17 +2,21 @@ import { Router, Request, Response } from "express";
 import { isUri } from "valid-url";
 import { baseURL } from "../lib/constants";
 import { generate } from "shortid";
+import { firestore } from "../lib/database";
 const router = Router();
 
 // @route       /api/url/shorten
 // @desc        Shorten URL
-router.post("/shorten", (req:Request, res:Response) => {
+router.post("/shorten", async (req: Request, res: Response) => {
     const { longUrl } = req.body;
 
     // Check base URL
     if (!isUri(baseURL)) {
         return res.status(401).json("Invalid Base URL");
     }
+
+    // Create URL code
+    const urlCode = generate();
 });
 
 export default router;

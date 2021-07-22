8dbed91fcb2e23df3e0460838ca122b5f0dc87d6
Naveed <46922100+nedanwr@users.noreply.github.com>
2021-07-22 14:49:19 +0500

    feat: Add check to see if user is an admin

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,5 @@
-import { Router, Request, Response } from "express";
+import { Router, Response } from "express";
+import verifyToken from "../lib/verifyToken";
 import { v4 as uuid } from "uuid";
 import jwt from "jsonwebtoken";
 import { jwtSecret } from "../lib/constants";
@@ -7,35 +8,49 @@ const router = Router();
 
 // @route       POST /api/user/create
 // @desc        Create new user & assign a json web token
-router.post("/create", async (req:Request, res:Response) => {
-    // Get email value from body
-    const { email } = req.body;
-    // Generate new uuid
-    const uid = uuid();
-    // JWT Payload
-    const payload = {
-        email,
-        uid,
-    };
-    // Generate JWT Token
-    const token = jwt.sign(payload, jwtSecret);
+router.post("/create", verifyToken, (req:any, res:Response) => {
+    // Verify token stored in req.token
+    jwt.verify(req.token, jwtSecret, async (err:any, authData:any) => {
+        // Send 403 if any error verifying token or if user is not authorized to access resource
+        if (err) {
+            res.sendStatus(403);
+       }
+       else {
+            firestore.collection("users").doc(authData.uid).get().then(async user => {
+                if (!user.data()?.administrator) {
+                    return res.sendStatus(403);
+                }
+                // Get user email value from body
+                const {email} = req.body;
+                // Generate new uuid
+                const uid = uuid();
+                // JWT Payload
+                const payload = {
+                    email,
+                    uid,
+                };
+                // Generate JWT Token
+                const token = jwt.sign(payload, jwtSecret);
 
-    try {
-        // Store values into firebase
-        await firestore.collection("users").doc(uid).set({
-            uid,
-            email,
-            administrator: false,
-            token,
-            createdAt: new Date(),
-        });
+                try {
+                    // Store values into firebase
+                    await firestore.collection("users").doc(uid).set({
+                        uid,
+                        email,
+                        administrator: false,
+                        token,
+                        createdAt: new Date(),
+                    });
 
-        res.status(201).json("User Created");
-    }
-    catch (err) {
-        console.error(err.stack);
-        res.status(500).json("Internal Server Error");
-    }
+                    res.status(201).json("User Created");
+                } catch (err) {
+                    // If error, log error message & send 500 status code
+                    console.error(err.stack);
+                    res.status(500).json("Internal Server Error");
+                }
+            });
+       }
+    });
 });
 
 export default router;
